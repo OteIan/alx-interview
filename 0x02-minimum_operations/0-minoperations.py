4d4d8aedd95fcb4603e0a6a1753ea0c31fbd652a
OteIan <oteian9@gmail.com>
2024-03-24 21:45:25 +0300

    Min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Minimum operations module
+"""
+
+def minOperations(n):
+    """
+    Minimum operations method
+    """
+    count = 0
+    clipboard = 0
+    done = 1
+
+    while done < n:
+        if clipboard == 0:
+            # First copy all and paste
+            clipboard = done
+            done += clipboard
+            count += 2
+
+        elif n - done > 0 and (n - done) % done == 0:
+            # Copy all and paste
+            clipboard = done
+            done += clipboard
+            count += 2
+
+        elif clipboard > 0:
+            # Paste
+            done += clipboard
+            count += 1
+
+    return count
