6c603962aab093482af3d84c30fa1e7206808475
OteIan <oteian9@gmail.com>
2024-03-28 12:32:07 +0300

    Fix: new changes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,41 +5,40 @@ import sys
 
 line_count = 0
 total_file_size = 0
-status_code_count = {200: 0, 301: 0, 400: 0, 401: 0,
-                     403: 0, 404: 0, 405: 0, 500: 0}
+status_code_count = {"200": 0, "301": 0, "400": 0, "401": 0,
+                     "403": 0, "404": 0, "405": 0, "500": 0}
 
-try:
-    for line in sys.stdin:
-        # Strip the output into parts
-        output = line.strip().split(" ")
-
-        if len(line) != 7:
-            continue
-
-        # Obtain the status code and file size
-        status_code = int(output[-2])
-        file_size = int(output[-1])
-
-        # Increment the status code count by 1
-        if status_code in status_code_count and isinstance(status_code, int):
-            status_code_count[status_code] += 1
-
-            # Increment the line count and total file size
-            line_count += 1
-            total_file_size += file_size
-
-        # Print the metrics after 10 lines of data has been processed
-        if line_count % 10 == 0:
-            print(f"File size: {total_file_size}")
-            for code in sorted(status_code_count.keys()):
-                if status_code_count[code] > 0:
-                    print(f"{code}: {status_code_count[code]}")
-
-except KeyboardInterrupt:
-    # Print the metrics if Ctrl+C is called
+def print_metrics(status_code_count, total_file_size):
+    """
+    Prints the metrics
+    """
     print(f"File size: {total_file_size}")
     for code in sorted(status_code_count.keys()):
         if status_code_count[code] > 0:
             print(f"{code}: {status_code_count[code]}")
+
+try:
+    for line in sys.stdin:
+        output = line.split()
+
+        if len(output) > 2:
+            line_count += 1
+
+            if line_count <= 10:
+                status_code = output[-2]
+                total_file_size += int(output[-1])
+
+                if status_code in status_code_count.keys():
+                    status_code_count[status_code] += 1
+
+
+            if line_count == 10:
+                print_metrics(status_code_count, total_file_size)
+                line_count = 0
+            
+
+except KeyboardInterrupt:
+    # Print the metrics if Ctrl+C is called
+    print_metrics(status_code_count, total_file_size)
 
     sys.exit(0)
