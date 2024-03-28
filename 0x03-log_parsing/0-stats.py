8a14b8444427b9b4395e1cba04d930d7361bbee2
OteIan <oteian9@gmail.com>
2024-03-28 12:12:09 +0300

    Fix: PEP8

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,8 @@ import sys
 
 line_count = 0
 total_file_size = 0
-status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_code_count = {200: 0, 301: 0, 400: 0, 401: 0,
+                     403: 0, 404: 0, 405: 0, 500: 0}
 
 try:
     for line in sys.stdin:
@@ -16,13 +17,13 @@ try:
         status_code = output[-2]
         file_size = int(output[-1])
 
-        # Increment the line count and total file size
-        line_count += 1
-        total_file_size += file_size
-
         # Increment the status code count by 1
         if status_code in status_code_count and isinstance(status_code, int):
             status_code_count[status_code] += 1
+
+            # Increment the line count and total file size
+            line_count += 1
+            total_file_size += file_size
 
         # Print the metrics after 10 lines of data has been processed
         if line_count % 10 == 0:
@@ -32,7 +33,7 @@ try:
                     print(f"{code}: {status_code_count[code]}")
 
 except KeyboardInterrupt:
-    #Print the metrics if Ctrl+C is called
+    # Print the metrics if Ctrl+C is called
     print(f"File size: {total_file_size}")
     for code in sorted(status_code_count.keys()):
         if status_code_count[code] > 0:
