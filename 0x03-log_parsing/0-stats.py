09d3f92bebaa7899a4fb5983198846bb370a1c7f
OteIan <oteian9@gmail.com>
2024-03-28 12:44:21 +0300

    Fix: if lines are 10

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,7 +11,13 @@ status_code_count = {"200": 0, "301": 0, "400": 0, "401": 0,
 
 def print_metrics(status_code_count, total_file_size):
     """
-    Prints the metrics
+    Prints the metrics including total file size and the number of lines for
+    each status code.
+
+    Args:
+        status_code_count (dict): Dictionary containing status codes as keys
+        and their counts as values.
+        total_file_size (int): Total file size accumulated so far.
     """
     print(f"File size: {total_file_size}")
     for code, number in sorted(status_code_count.items()):
@@ -20,23 +26,23 @@ def print_metrics(status_code_count, total_file_size):
 
 
 try:
+    # Read stdin line by line
     for line in sys.stdin:
         output = line.split()
 
         if len(output) > 2:
             line_count += 1
 
-            if line_count <= 10:
-                status_code = output[-2]
-                total_file_size += int(output[-1])
+            status_code = output[-2]
+            total_file_size += int(output[-1])
 
-                if status_code in status_code_count.keys():
-                    status_code_count[status_code] += 1
+            if status_code in status_code_count.keys():
+                status_code_count[status_code] += 1
 
-            if line_count == 10:
+            if line_count % 10 == 0:
+                # Print metrics after every 10 lines
                 print_metrics(status_code_count, total_file_size)
-                line_count = 0
 
 finally:
-    # Print the metrics if Ctrl+C is called
+    # Print the final metrics if Ctrl+C is called
     print_metrics(status_code_count, total_file_size)
