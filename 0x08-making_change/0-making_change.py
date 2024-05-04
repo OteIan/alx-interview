9203e83e791e039a802275d1ce12f2e6de81f7fc
OteIan <oteian9@gmail.com>
2024-05-04 19:56:05 +0300

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Making change module
+"""
+
+
+def makeChange(coins, total):
+    """
+    Make change function
+    """
+    if total <= 0 or min(coins) > total:
+        return 0
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            change += 1
+
+    if total != 0:
+        return -1
+
+    return change
