ea21246c266fdad5b6305e272a97073301854f6c
OteIan <oteian9@gmail.com>
2024-05-10 00:50:09 +0300

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Island perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """
+    Island perimeter method
+    """
+    sum = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                sum += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    sum -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    sum -= 2
+
+    return sum
